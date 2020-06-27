edafb22b6ec1df1d4e3fa07c926af3f47df4bf27
Anasu <misushima@gmail.com>
2020-06-27 14:17:23 -0400

    Se instala font awesome con los íconos a utilizar

--- src/main.js
+++ src/main.js
@@ -13,10 +13,10 @@ Vue.use(BootstrapVueIcons)
 
 // Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFacebook, faGithub} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUserSecret)
+library.add(faTwitter, faFacebook, faGithub)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
