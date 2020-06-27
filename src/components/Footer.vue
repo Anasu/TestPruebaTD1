83d3e95c0b524649aecbd1225d958000d6c4ea12
Anasu <misushima@gmail.com>
2020-06-27 13:33:43 -0400

    Se testea font awesome

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
